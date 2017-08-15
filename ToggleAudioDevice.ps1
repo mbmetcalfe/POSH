6af39414e41f7e3a4ab323fe6e94d3fe75c1c130
mbmetcalfe <30930387+mbmetcalfe@users.noreply.github.com>
2017-08-15 14:50:34 -0300

    Making generic
    Making the script a bit more generic by moving around some hard-coded paths.

--- ToggleAudioDevice.ps1
+++ ToggleAudioDevice.ps1
@@ -3,7 +3,8 @@
     This script is used to toggle the current audio device.
 
     .DESCRIPTION 
-    This script is used to toggle the current audio device.
+    This script is used to toggle the current audio device.  It uses an environment variable
+    to keep track of which one is active.
 
     .NOTES 
     Author: Michael Metcalfe
@@ -11,11 +12,17 @@
 
     Set $AUDIO_DEVICE1 and $AUDIO_DEVICE2 to the 2 audio devices to toggle 
     between.  These names can be found/renamed in the audio settings. 
-    This script uses a  3rd party software to display the toaster notifications of which device was set: 
+    This script uses a  3rd party software to set the audio device of which device was set: 
     http://www.nirsoft.net/utils/nircmd.html
     If you wish not to have this 3rd party software, remove the lines referencing 'nircmd'.
 #>
-Import-Module C:\Users\micha\OneDrive\Documents\WindowsPowerShell\Modules\Toast.psm1
+$BasePath = "C:\dev\POSH"
+Import-Module "$BasePath\Modules\Toast.psm1"
+
+<# 
+    TODO: Set up a dictionary with the device name as the key and have the image as the value
+        or something similar.    
+#>
 $AUDIO_DEVICE1 = 'Speakers'
 $AUDIO_DEVICE2 = 'Headphones'
 
@@ -34,17 +41,18 @@ function Set-EnvironmentVariable
 }
 
 $currentAudioDevice = [System.Environment]::GetEnvironmentVariable('CurrentAudioDevice')
-$toastImage = "C:\Users\micha\OneDrive\Pictures\Avatars\Skeptical_Hippo.jpeg"
+# Default Toast Image
+$toastImage = [environment]::getfolderpath("MyPictures") + "\Avatars\Skeptical_Hippo.jpeg"
 
 if ($currentAudioDevice -eq $AUDIO_DEVICE1)
 {
     $newAudioDevice = $AUDIO_DEVICE2
-    $toastImage = "D:\Tools\Scripts\Steelseries_Headphones.png"
+    $toastImage = "$BasePath\Steelseries_Headphones.png"
 }
 elseif ($currentAudioDevice -eq $AUDIO_DEVICE2)
 {
     $newAudioDevice = $AUDIO_DEVICE1
-    $toastImage = "D:\Tools\Scripts\Bose_Speakers.jpg"
+    $toastImage = "$BasePath\Bose_Speakers.jpg"
 }
 else
 {
