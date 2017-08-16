ab7cde35ab421b56b252678c1ad97d14a627533a
mbmetcalfe <30930387+mbmetcalfe@users.noreply.github.com>
2017-08-15 23:49:55 -0300

    Update
    Incorporating some updates made previously.  Gathering scripts from other locations and merging/updating changes.

---
+++ Modules/Get-Temperature.psm1
@@ -0,0 +1,26 @@
+﻿function Get-Temperature {
+    try
+    {
+        $t = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+    }
+    catch
+    {
+        Write-Error "Error obtaining temperature."
+    }
+
+    $currentTempKelvin = $t.CurrentTemperature / 10
+    $currentTempCelsius = $currentTempKelvin - 273.15
+
+    $currentTempFahrenheit = (9/5) * $currentTempCelsius + 32
+
+    return $currentTempCelsius.ToString() + " C : " + $currentTempFahrenheit.ToString() + " F : " + $currentTempKelvin + "K"  
+}
+
+# Save in your c:\users\yourName\Documents\WindowsPowerShell\modules\ directory
+# in sub directory get-temperature as get-temperature.psm1
+# You **must** run as Administrator.
+# It will only work if your system & BIOS support it. If it doesn't work, I can't help you.
+
+# Just type get-temperature in PowerShell and it will spit back the temp in Celsius, Farenheit and Kelvin.
+
+# More info on my blog: http://ammonsonline.com/is-it-hot-in-here-or-is-it-just-my-cpu/
