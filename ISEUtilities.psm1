c95b57530c8cf2ede62dcfe271fcba265fda7d04
mbmetcalfe <30930387+mbmetcalfe@users.noreply.github.com>
2017-08-22 14:15:00 -0300

    Format-Document
    Added cmdlet Format-Document.

---
+++ ISEUtilities.psm1
@@ -0,0 +1,274 @@
+﻿Import-Module $PSScriptRoot\IOUTilties.psm1
+
+$DefaultISESessionFile = ([Environment]::GetFolderPath("MyDocuments") + "\files.isexml")
+
+function Save-AllISEFiles
+{
+    <#
+    .SYNOPSIS
+        Saves all ISE Files except for untitled files. If You have multiple PowerShellTabs, saves files in all tabs.
+    #>
+    foreach ($tab in $psISE.PowerShellTabs)
+    {
+        foreach ($file in $tab.Files)
+        {
+            if (!$file.IsUntitled)
+            {
+                $file.Save()
+            }
+        }
+    }
+}
+
+function Close-AllISEFiles
+{
+    <#
+    .SYNOPSIS
+        Close all ISE Files except for untitled files. If You have multiple PowerShellTabs, close files in all tabs.
+    #>
+#    foreach ($tab in $psISE.PowerShellTabs)
+#    {
+##        $psISE.PowerShellTabs.Remove($tab)
+#        foreach ($file in $tab.Files)
+#        {
+##            if (!$file.IsUntitled)
+##            {
+##                $file.Save()
+##            }
+            
+#        }
+#    }
+
+    Save-AllISEFiles
+    $psISE.CurrentPowerShellTab.Files.Clear()
+}
+
+function Export-ISEState
+{
+    <#
+    .SYNOPSIS
+        Stores the opened files in a serialized xml so that later the same set can be opened
+    .DESCRIPTION
+        Creates an xml file with all PowerShell tabs and file information
+    .PARAMETER fileName
+        The name of the project to create a new version from. This will also be the name of the new project, but with a different version
+    .EXAMPLE
+        Stores current state into c:\temp\files.isexml
+        Export-ISEState c:\temp\files.isexml
+    #>
+    param
+    (
+        [Parameter(Position = 0,Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$FileName
+    )
+    <#
+        We are exporting a “tree” worth of information like this:
+
+        SelectedTabDisplayName: PowerShellTab 1
+        SelectedFilePath: c:\temp\a.ps1
+        TabInformation:
+            PowerShellTab 1:
+                File 1:
+                    FullPath:     c:\temp\a.ps1
+                    FileContents: $null
+                File 2:
+                    FullPath:     Untitled.ps1
+                    FileContents: $a=0…
+            PowerShellTab 2:
+            …
+        Hashtables and arraylists serialize rather well with export-clixml
+        We will keep the list of PowerShellTabs in one ArrayList and the list of files
+        and contents(for untitled files) inside each tab in a couple of ArrayList.
+        We will use Hashtables to group the information.
+    #>
+    $tabs = New-Object collections.arraylist
+    # before getting file information, save all untitled files to make sure their latest text is on disk
+    Save-AllISEFiles
+    foreach ($tab in $psISE.PowerShellTabs)
+    {
+        $files = New-Object collections.arraylist
+        $filesContents = New-Object collections.arraylist
+        foreach ($file in $tab.Files)
+        {
+            # $null = will avoid $files.Add from showing in the output
+            $null = $files.Add($file.FullPath)
+            if ($file.IsUntitled)
+            {
+                # untitled files are not yet on disk so we will save the file contents inside the xml
+                # export-clixml performs the appropriate escaping for the contents to be inside the xml
+                $null = $filesContents.Add($file.Editor.Text)
+            }
+            else
+            {
+                # titled files get their content from disk
+                $null = $filesContents.Add($null)
+            }
+        }
+        $simpleTab = New-Object collections.hashtable
+        # The DisplayName of a PowerShellTab can only be change with scripting
+        # we want to maintain the chosen name
+        $simpleTab[“DisplayName”] = $tab.DisplayName
+        # $files and $filesContents is the information gathered in the foreach $file above
+        $simpleTab[“Files”] = $files
+        $simpleTab[“FilesContents”] = $filesContents
+        # add to the list of tabs
+        $null = $tabs.Add($simpleTab)
+    }
+    # tabsToSerialize will be a hashtable with all the information we want
+    # it is the “root” of the information to be serialized in the hashtable we store…
+    $tabToSerialize = New-Object collections.hashtable
+    # the $tabs information gathered in the foreach $tab above…
+    $tabToSerialize[“TabInformation”] = $tabs
+    # …and the selected tab and file.
+    $tabToSerialize[“SelectedTabDisplayName”] = $psISE.CurrentPowerShellTab.DisplayName
+    $tabToSerialize[“SelectedFilePath”] = $psISE.CurrentFile.FullPath
+    # now we just export it to $FileName
+    $tabToSerialize | Export-Clixml -Path $FileName
+
+    Write-Host "Saved session to $FileName." -ForegroundColor DarkMagenta
+}
+
+function Import-ISEState
+{
+    <#
+    .SYNOPSIS
+        Reads a file with ISE state information about which files to open and opens them
+    .DESCRIPTION
+        Reads a file created by Export-ISEState with the PowerShell tabs and files to open
+    .PARAMETER fileName
+        The name of the file created with Export-ISEState
+    .EXAMPLE
+        Restores current state from c:\temp\files.isexml
+        Import-ISEState c:\temp\files.isexml
+    #>
+    param
+    (
+        [Parameter(Position = 0,Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$FileName
+    )
+
+    if (!(Test-Path $FileName))
+    {
+        Write-Error "ISE State File, '$FileName', does not exist."
+        return ""
+    }
+
+    Write-Host "Restoring session from $FileName..." -ForegroundColor DarkMagenta
+    <# currentTabs is used to keep track of the tabs currently opened.
+        If “PowerShellTab 1” is opened and $FileName contains files for it, we
+        want to open them in “PowerShellTab 1” #>
+    $currentTabs = New-Object collections.hashtable
+    foreach ($tab in $psISE.PowerShellTabs)
+    {
+        $currentTabs[$tab.DisplayName] = $tab
+    }
+    $tabs = Import-Clixml -Path $FileName
+    # those will keep track of selected tab and files
+    $selectedTab = $null
+    $selectedFile = $null
+    foreach ($tab in $tabs.TabInformation)
+    {
+        $newTab = $currentTabs[$tab.DisplayName]
+        if ($newTab -eq $null)
+        {
+            $newTab = $psISE.PowerShellTabs.Add()
+            $newTab.DisplayName = $tab.DisplayName
+        }
+        <# newTab now has a brand new or a previouslly existing PowerShell tab with the same name as the one in the file
+            if the tab is the selected tab save it for later selection. #>
+        if ($newTab.DisplayName -eq $tabs.SelectedTabDisplayName)
+        {
+            $selectedTab = $newTab
+        }
+        <# currentUntitledFileContents keeps track of the contents for untitled files
+            if you already have the content in one of your untitled files there 
+            is no reason to add the same content again this will make sure calling 
+            import-ISEState multiple times does not keep on adding untitled files 
+            $currentUntitledFileContents = New-Object collections.hashtable #>
+        foreach ($newTabFile in $newTab.Files)
+        {
+            if ($newTabFile.IsUntitled)
+            {
+                $currentUntitledFileContents[$newTabFile.Editor.Text] = $newTabFile
+            }
+        }
+        # since we will want both file and fileContents we need to use a for instead of a foreach
+        for ($i = 0; $i -lt $tab.Files.Count; $i++)
+        {
+            $file = $tab.Files[$i]
+            $fileContents = $tab.FilesContents[$i]
+            #fileContents will be $null for titled files
+            if ($fileContents -eq $null)
+            {
+                # the overload of Add taking one string opens the file identified by the string
+                $newFile = $newTab.Files.Add($file)
+            }
+            else # the file is untitled
+            {
+                #see if the content is already present in $newTab
+                $newFile = $currentUntitledFileContents[$fileContents]
+                if ($newFile -eq $null)
+                {
+                    <# the overload of Add taking no arguments creates a new untitled file
+                        The number for untitled files is determined by the application so we
+                        don’t try to keep the untitled number, we just create a new untitled. #>
+                    $newFile = $newTab.Files.Add()
+                    # and here we restore the contents
+                    $newFile.Editor.Text = $fileContents
+                }
+            }
+            # if the file is the selected file in the selected tab save it for later selection
+            if (($selectedTab -eq $newTab) -and ($tabs.SelectedFilePath -eq $file))
+            {
+                $selectedFile = $newFile
+            }
+        }
+    }
+    #finally we selected the PowerShellTab that was selected and the file that was selected on it.
+    $psISE.PowerShellTabs.SetSelectedPowerShellTab($selectedTab)
+    if ($selectedFile -ne $null)
+    {
+        $selectedTab.Files.SetSelectedFile($selectedFile)
+    }
+}
+
+
+# Add a new option in the Add-ons menu to save session.
+if (!($psISE.CurrentPowerShellTab.AddOnsMenu.Submenus | Where-Object { $_.DisplayName -eq "Save ISE Session" }))
+{
+    $psise.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Save ISE Session", {Export-ISEState -FileName ([Environment]::GetFolderPath("MyDocuments") + "\files.isexml")} , "Ctrl+Alt+S") | Out-Null
+#    $psise.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Save ISE Session", {Export-ISEState -FileName $Script:$DefaultISESessionFile} , "Ctrl+Alt+S") | Out-Null
+}
+
+# Add a new option in the Add-ons menu to save session.
+if (!($psISE.CurrentPowerShellTab.AddOnsMenu.Submenus | Where-Object { $_.DisplayName -eq "Save ISE Session As..." }))
+{
+    $psise.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Save ISE Session As...", {Export-ISEState -FileName (Save-FileAsDialog -Filter "XML Files|*.xml|All Files|*.*")} , $null) | Out-Null
+}
+
+# Add a new option in the Add-ons menu to restore session.
+if (!($psISE.CurrentPowerShellTab.AddOnsMenu.Submenus | Where-Object { $_.DisplayName -eq "Restore ISE Session" }))
+{
+    $psise.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Restore ISE Session", {Close-AllISEFiles; Import-ISEState -FileName ([Environment]::GetFolderPath("MyDocuments") + "\files.isexml")} , "Ctrl+Alt+R") | Out-Null
+#    $psise.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Restore ISE Session", {Close-AllISEFiles; Import-ISEState -FileName $DefaultISESessionFile} , "Ctrl+Alt+R") | Out-Null
+}
+
+# Add a new option in the Add-ons menu to open a specific session.
+if (!($psISE.CurrentPowerShellTab.AddOnsMenu.Submenus | Where-Object { $_.DisplayName -eq "Open ISE Session..." }))
+{
+    $psise.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Open ISE Session...", {Close-AllISEFiles; Import-ISEState -FileName (Open-FileDialog -Filter "XML Files|*.xml|All Files|*.*")} , "Ctrl+Alt+O") | Out-Null
+}
+
+# Add a new option in the Add-ons menu to save all opened files.
+if (!($psISE.CurrentPowerShellTab.AddOnsMenu.Submenus | Where-Object { $_.DisplayName -eq "Save All Files" }))
+{
+    $psise.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Save All Files", {Save-AllISEFiles} , $null) | Out-Null
+}
+
+# Add a new option in the Add-ons menu to close all opened files.
+if (!($psISE.CurrentPowerShellTab.AddOnsMenu.Submenus | Where-Object { $_.DisplayName -eq "Close All Files" }))
+{
+    $psise.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Close All Files", {Close-AllISEFiles} , "Ctrl+Alt+X") | Out-Null
+}
---
+++ Miscellaneous.psm1
@@ -0,0 +1,433 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$SendPhoneConfigFile = ([Environment]::GetFolderPath("MyDocuments") + "\send_phone.xml")
+
+function Block-Idle
+{
+    <#
+    .SYNOPSIS
+    Move the mouse a random amount in X/Y.
+
+    .DESCRIPTION
+    This script will move the mouse a random amount from its current location at a set interval for a set amount of time.
+
+    .PARAMETER Duration
+    The amount of time, in minutes to perform the mouse-move.
+
+    .PARAMETER  ActionTime
+    The amount of time, in seconds, between each interval of moving the mouse.
+
+    .PARAMETER  Silent
+    Whether or not to display output.  If true, no output is displayed.
+
+    .EXAMPLE
+    PS C:\> Block-Idle -Duration 1 -ActionTime 5
+    Will move the mouse every 5 seconds for 1 minute.
+
+    .NOTES
+    NAME        :  Block-Idle
+    VERSION     :  1.0   
+    LAST UPDATED:  6/5/2017
+    AUTHOR      :  Michael Metcalfe
+    .INPUTS
+    None
+    .OUTPUTS
+    None
+    #>
+    [CmdletBinding()]
+    param (
+        $Duration = 1,  # How long (in minutes) to perform the action
+        $ActionTime = 5, # How often (in seconds) to perform the action
+        [switch]$Silent = $false # Whether or not to display output.
+    )
+
+    $myshell = New-Object -com "Wscript.Shell"
+
+    if (!$Silent) {Write-Host (("Will perform every {0} seconds for {1} minutes.") -f ($ActionTime, $Duration))}
+
+    $MaxXMovement = 200
+    $MaxYMovement = 200
+
+    $DurationSeconds = $Duration * 60
+    for ($i = 0; $i -lt $DurationSeconds; $i+=$ActionTime)
+    {
+        Start-Sleep -Seconds $ActionTime
+
+        $Pos = [System.Windows.Forms.Cursor]::Position
+    
+        # Set X and Y to the current position +/- (0 to Max Movement)
+        $newXOffset = (Get-Random -Minimum -1 -Maximum 2) * (Get-Random -Minimum 0 -Maximum $MaxXMovement)
+        $newYOffset = (Get-Random -Minimum -1 -Maximum 2) * (Get-Random -Minimum 0 -Maximum $MaxYMovement)
+
+        $newX = $Pos.X + $newXOffset
+        $newY = $Pos.Y + $newYOffset
+        Write-Debug (("({0}, {1}) -> ({2}, {3})") -f ($Pos.X,$Pos.Y,$newX,$newY))
+        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($newX) , ($newY))
+    }
+}
+New-Alias -Name noidle -Value Block-Idle -Description "Keep PC from going idle."
+
+function Show-Pause
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$Message
+    )
+
+    # Check if running Powershell ISE
+    if ($psISE)
+    {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.MessageBox]::Show("$Message")
+    }
+    else
+    {
+        Write-Host "$Message " -ForegroundColor Yellow -NoNewline
+        $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+        Write-Host ""
+    }
+}
+New-Alias -Name spause -Value Show-Pause -Description "Show a pause message or dialog."
+
+function Clear-Session()
+{
+    <#
+        .SYNOPSIS
+        Clear variables and modules.
+
+        .DESCRIPTION
+        This cmdlet will clear all variables and modules from the current console session.
+
+        .PARAMETER ClearVariables
+        Clear variables from the current session.
+
+        .PARAMETER  ClearModules
+        Clear modules from the current session.
+
+        .PARAMETER ReloadProfile
+        After clearing items, load the default PowerShell profile file.
+
+        .EXAMPLE
+        PS C:\> Clear-Session
+        Will clear all variables and modules from the current console session.
+
+        .EXAMPLE
+        PS C:\> Clear-Session -ClearVariables
+        Will clear all variables current console session.
+
+        .NOTES
+        NAME        :  Clear-Session
+        VERSION     :  1.0   
+        LAST UPDATED:  7/12/2017
+        AUTHOR      :  Michael Metcalfe
+        .INPUTS
+        None
+        .OUTPUTS
+        None
+    #>
+    [CmdletBinding()]
+    param
+    (
+        [switch]$ClearVariables,
+        [switch]$ClearModules,
+        [switch]$ReloadProfile
+    )
+
+    # if neither parameter supplied, default to both enabled.
+    if (-not $ClearVariables -and -not $ClearModules)
+    {
+        $ClearVariables = $true
+        $ClearModules = $true
+    }
+
+    if ($ClearModules)
+    {
+        $moduleCount = (Get-Module | Measure).Count
+        Remove-Module *
+        $moduleCount = $moduleCount - ((Get-Module | Measure).Count)
+        Write-Host "$moduleCount modules cleared." -ForegroundColor Green
+    }
+    
+    if ($ClearVariables)
+    {
+        Remove-Variable * -ErrorAction SilentlyContinue -Exclude "ClearVariables"
+        Write-Host "Variables cleared." -ForegroundColor Green
+    }
+    $error.Clear()
+
+    if ($ReloadProfile)
+    {
+        Write-Host "Reloading profile: $profile" -ForegroundColor Yellow
+        & "$profile"
+    }
+}
+
+function Send-PhoneCommand
+{
+    <#
+        .SYNOPSIS
+        Perform a Tasker command on a specific phone.
+
+        .DESCRIPTION
+        Using Join's API, send a Tasker command to an identified phone.  For this to work, Tasker has
+        to be setup to accept a command via the Join push.
+        See this URL on how to set this up: http://forum.joaoapps.com/index.php?resources/run-any-task-from-any-autoapp.139/
+
+        .PARAMETER PhoneName
+        The friendly name of the phone that the command will get sent to.
+
+        .PARAMETER DeviceName
+        The device name of the phone that the command will get sent to.
+
+        .PARAMETER  Command
+        The Tasker command to execute.
+
+        .EXAMPLE
+        PS C:\> Send-PhoneCommand -PhoneName Home -Command "Work Mode On"
+        Execute the Tasker command "Work Mode On" on the "Home" phone.
+
+        .NOTES
+        NAME        :  Send-PhoneCommand
+        VERSION     :  1.0   
+        LAST UPDATED:  18/08/2017
+        AUTHOR      :  Michael Metcalfe
+        .INPUTS
+        None
+        .OUTPUTS
+        None
+    #>
+    [CmdletBinding(DefaultParameterSetName='ByPhoneName')]
+    param
+    (
+        [Parameter(Mandatory = $true, ParameterSetName = 'ByPhoneName')]
+        [string]$PhoneName,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'ByDeviceName')]
+        [string]$DeviceName,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Command,
+
+        [switch]$Force
+    )
+
+    if (!(Test-Path $SendPhoneConfigFile))
+    {
+        Write-Error (("Could not find input file '{0}'.") -f ($SendPhoneConfigFile))
+        return $null
+    }
+
+    [xml]$ConfigFile = Get-Content $SendPhoneConfigFile
+
+    $SendPhoneAPIKey = [string]$ConfigFile.config.APIKey
+
+    # Get the actual device name to send to.
+    if ($PhoneName -and !$DeviceName)
+    {
+        $DeviceName = ($ConfigFile.config.phones.phone | where name -eq $PhoneName).device_name        
+    }
+
+    # If user asks to Force the command we don't need to lookup if the command is valid or not.
+    if (!$Force)
+    {
+        # To check if "command" is a command in at least one of the phones' list:
+        # $ConfigFile.config.phones.phone.commands.command -contains "COMMAND"
+        if (!((Get-PhoneCommands -DeviceName $DeviceName) -contains $Command))
+        {
+            Write-Error "$Command is not a valid command for $(if ($PhoneName) {$PhoneName} elseif ($DeviceName) {$DeviceName} else {" ?? "})"
+            return $null
+        }
+    }
+
+    $null = Invoke-WebRequest -Uri (("https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?deviceNames={0}&text=task%3D%3A%3D{1}&apikey={2}") -f ($DeviceName, $Command, $SendPhoneAPIKey)) -Method Post
+    # To send to all phones, use this URL:
+    #https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?deviceId=group.phone&text=task%3D%3A%3DCOMMAND&apikey=API_KEY
+}
+New-Alias -Name phonecmd -Value Send-PhoneCommand -Description "Send a Tasker command to a phone."
+New-Alias -Name spc -Value Send-PhoneCommand -Description "Send a Tasker command to a phone."
+
+function Get-PhoneCommands
+{
+    <#
+        .SYNOPSIS
+        Get support list of Tasker commands on a specific phone.
+
+        .DESCRIPTION
+        Get the list of possible commands to send with the Send-PhoneCommand cmdlet.
+        The commands are stored in "MyDocuments\send_phone.xml".
+
+        .PARAMETER PhoneName
+        The friendly name of the phone being queried.
+
+        .PARAMETER DeviceName
+        The device name of the phone being queried.
+
+        .EXAMPLE
+        PS C:\> Get-PhoneCommands -PhoneName Home
+        Get the list of commands for the "Home" phone.
+
+        .EXAMPLE
+        PS C:\> Get-PhoneCommands -DeviceName
+        Get the list of commands for the "SM-J320W8" device.
+
+        .NOTES
+        NAME        :  Get-PhoneCommands
+        VERSION     :  1.0   
+        LAST UPDATED:  18/08/2017
+        AUTHOR      :  Michael Metcalfe
+        .INPUTS
+        This command requires a configuration file with the phones and commands in it.  
+        It should be stored in "MyDocuments\send_phone.xml".  The general format is as follows:
+
+            <config>
+              <APIKey>abcdef1234hasdf</APIKey>
+              <phones>
+                <phone name='Home' device_name='Galaxy S8'>
+                    <commands>
+                        <command>Home Mode On</command>
+                        <command>Home Mode Off</command>
+                        <command>Turn On The Lights</command>
+                        <command>Turn Off The Lights</command>
+                    </commands>
+                </phone>
+
+                <phone name='Work' device_name='SM-J320W8'>
+                    <commands>
+                        <command>Work Mode On</command>
+                        <command>Work Mode Off</command>
+                        <command>Meeting Mode</command>
+                        <command>Mute</command>
+                        <command>UnMute</command>
+                    </commands>
+                </phone>
+              </phones>
+            </config>
+
+        .OUTPUTS
+        None
+    #>
+    [CmdletBinding(DefaultParameterSetName='ByPhoneName')]
+    param
+    (
+        [Parameter(Mandatory = $true, ParameterSetName = 'ByPhoneName')]
+        [string]$PhoneName,
+        [Parameter(Mandatory = $true, ParameterSetName = 'ByDeviceName')]
+        [string]$DeviceName
+    )
+    
+    if (!(Test-Path $SendPhoneConfigFile))
+    {
+        Write-Error (("Could not find input file '{0}'.") -f ($SendPhoneConfigFile))
+        return $null
+    }
+
+    [xml]$ConfigFile = Get-Content $SendPhoneConfigFile
+    if ($PhoneName)
+    {
+        $commands = ($ConfigFile.config.phones.phone | where name -eq $PhoneName).commands
+    }
+    elseif ($DeviceName)
+    {
+        $commands = ($ConfigFile.config.phones.phone | where device_name -eq $DeviceName).commands
+    }
+    else
+    {
+        $commands = $null
+    }
+
+    if (!$commands)
+    {
+        Write-Debug "No commands found for $(if ($PhoneName) {$PhoneName} elseif ($DeviceName) {$DeviceName} else {" ?? "})."
+        return $null
+    }
+    else
+    {
+        return $commands.command
+    }
+}
+New-Alias -Name gpc -Value Get-PhoneCommands -Description "Get list of available Tasker commands that can be sent to a phone."
+
+Function Format-Document
+{
+    <#
+        .SYNOPSIS
+        Fix the indentation in the document.
+
+        .DESCRIPTION
+        Adjust all the indentation in the document.  If there is text selected, the formatting will only
+        be applied to the selected text.
+
+        .PARAMETER Spaces
+        The number of spaces used for a level of indentation.
+
+        .NOTES
+        NAME        :  Format-Document
+        VERSION     :  1.0   
+        LAST UPDATED:  22/08/2017
+        AUTHOR      :  Michael Metcalfe
+        Source      :  http://rnicholson.net/powershell-ise-formating-the-document/
+        .INPUTS
+        None
+        .OUTPUTS
+        None
+    #>
+    Param
+    (
+        [int]$Spaces = 4
+    )
+    $tab = " " * $Spaces
+    [int]$numtab = 0
+
+    # Grab selected text by default.
+    $text = $psISE.CurrentFile.editor.SelectedText
+    # If selected text is empty, use the entire document
+    if ($text.length -eq 0)
+    {
+        $text = $psISE.CurrentFile.editor.Text
+    }
+    else
+    {
+        # Determine how much indent there is at the beginning of the selected text
+        # and use it as the baseline for the indentation.
+        $numSpaces = $psISE.CurrentFile.Editor.SelectedText.Length - $psISE.CurrentFile.Editor.SelectedText.TrimStart().Length
+        # Assume there was extra spaces after desired indentation
+        $numtab = [Math]::Floor($numSpaces / $Spaces)
+    }
+
+    foreach ($l in $text -split [environment]::newline)
+    {
+        $line = $l.Trim()
+        
+        if ($line.StartsWith("}") -or $line.EndsWith("}"))
+        {
+            $numtab -= 1
+        }
+
+        $tab = " " * (($Spaces) * $numtab)
+        if($line.StartsWith(".") -or $line.StartsWith("< #") -or $line.StartsWith("#>"))
+        {
+            $tab = $tab.Substring(0, $tab.Length - 1)
+        }
+
+        $newText += "{0}{1}" -f (($tab) + $line),[environment]::newline
+        if ($line.StartsWith("{") -or $line.EndsWith("{"))
+        {
+            $numtab += 1
+        }
+        if ($numtab -lt 0)
+        {
+            $numtab = 0
+        }
+    }
+
+    if ($psISE.CurrentFile.editor.SelectedText.Length -eq 0)
+    {
+        $psISE.CurrentFile.Editor.Clear()
+    }
+    $psISE.CurrentFile.Editor.InsertText($newText)
+}
+
+#ls *.log | Select-String @("^.{2,3}-\d{4,5}", "exit code: (1|2|3|4)")
+Export-ModuleMember -Function * -Alias *
