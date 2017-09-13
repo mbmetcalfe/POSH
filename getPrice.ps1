456cedea6c020a9a952c86ea31f3350f1930b8c5
mbmetcalfe <30930387+mbmetcalfe@users.noreply.github.com>
2017-09-13 10:31:19 -0300

    Working with no saved output
    Retrieving all data, but the results not saved yet.

--- getPrice.ps1
+++ getPrice.ps1
@@ -1,42 +1,54 @@
-﻿$inputFile = "C:\Users\MBMETCAL\Downloads\LEGO Sets - Complete.csv"
-#$url = "https://brickset.com/sets/70001";
-#$url = "https://www.bricklink.com/v2/catalog/catalogitem.page?S=70001-1#T=P"
+﻿$inputFile = "C:\Users\MBMETCAL\Downloads\LEGO Sets - Complete.csv";
+$errorFile = $inputFile.Replace("Complete", "Error");
 
-$LEGOList = Import-Csv $inputFile
+#$bricksetURL = "https://brickset.com/sets/70001";
+#$bricksetURL = "https://www.bricklink.com/v2/catalog/catalogitem.page?S=70001-1#T=P"
+
+if (Test-Path $errorFile)
+{
+    Remove-Item $errorFile;
+}
+
+$LEGOList = Import-Csv $inputFile;
 ###reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /t REG_DWORD /v 1A10 /f /d 0
-
 
 foreach ($row in $LEGOList)
 {
-    #Write-Debug "Looking up price for $($row.Set) ($($row.'Set #'))";
     $content = $null;
-    $getContent = $null;
     try
     {
-        $url = "https://brickset.com/sets/{0}" -f ($($row.'Set #'));
+        $setNumber = $($row.'Set #');
+        $setName = $($row.Set);
+        $setSeries = $($row.Series);
+
+        $bricksetURL = "https://brickset.com/sets/{0}" -f ($setNumber);
+        $bricklinkURL = "https://www.bricklink.com/v2/catalog/catalogitem.page?S={0}#T=P" -f ($setNumber);
+        
         # Retrieve the web page for the set
-        $getContent = (iwr -Uri $url -UseBasicParsing);
-        #$content = ConvertTo-Xml -InputObject $content.RawContent;
-        $content = $getContent.Content;
+        $content = (iwr -Uri $bricksetURL);
+
         # parse the section that contains the prices
-###        $innerText = ($content.ParsedHtml.getElementsByTagName('a') | Where{ $_.className -eq 'plain' } ).innerText;
-        ## //*[@id="_idStoreResultListSection"]/table/tbody/tr[3]/td[5]/text()
+        $innerText = ($content.ParsedHtml.getElementsByTagName('a') | Where{ $_.className -eq 'plain' -and $_.hostname -eq "www.bricklink.com"} ).textContent
+        Write-Debug "Inner Text: $innerText"
+        $newPrice = if (($innerText[0]) -and ($innerText[0] -contains "$")) {$innerText[0]} else {"n/a"}
+        $usedPrice = if (($innerText[1]) -and ($innerText[1] -contains "$")) {$innerText[1]} else {"n/a"}
 
-
-        "$($row.Set) ($($row.'Set #')): New: {0}; Used: {1}" -f ($innerText[2], $innerText[3])
+        Write-Host ("{0} ({1}): New: {2}; Used: {3}; Link: {4}" -f ($setName, $setNumber, $newPrice, $usedPrice, $bricklinkURL))
     }
     catch
     {
-        Write-Host "Error retrieving price for $($row.Set): $_.ErrorDetails.Message." -ForegroundColor Red -BackgroundColor Black;
-        return;
+        '"{0}", "{1}", "{2}", "{3}", "{4}"' -f ($setName, $setSeries, $setNumber, $bricksetURL, $bricklinkURL) | Out-File -FilePath $errorFile -Append
+        Write-Host ("Error retrieving price for {0} ({1}): {2}." -f ($setName, $setNumber, $_)) -ForegroundColor Red -BackgroundColor Black;
     }
 }
+
+$LEGOList = $null
 
 ###reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v 1A10 /f
 
 #if (!$content)
 #{
-#    $content = (iwr -Uri $url);
+#    $content = (iwr -Uri $bricksetURL);
 #}
 ##$section = $content.ParsedHtml.getElementById('_idPGContents')
 #$innerText = ($content.ParsedHtml.getElementsByTagName('a') | Where{ $_.className -eq 'plain' } ).innerText | Where{ $_ -contains "$"}
