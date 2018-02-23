48d2a849217d9ff4659a9b9cfc1d83242ada0f32
Michael Metcalfe <michael.metcalfe@vac-acc.gc.ca>
2018-02-23 12:40:40 -0400

    Adding new cmdlet, Update-CSDN.

--- Modules/VAC-ACC.psm1
+++ Modules/VAC-ACC.psm1
@@ -36,7 +36,8 @@ function Set-CSDNDatabase
     $csdn = Get-ItemProperty -Path "HKLM:\Software\Wow6432Node\VAC-ACC\BRP\R2 csdn" -ErrorAction SilentlyContinue
     if($csdn.'Database Connect')
     {
-        Write-Host ("Changing CSDN Database from {0} to {1}." -f ($csdn.'Database Connect', $DatabaseName))
+        #Write-Host ("Changing CSDN Database from {0} to {1}." -f ($csdn.'Database Connect', $DatabaseName))
+        Write-Host -NoNewline ("Changing CSDN Database from {0} to {1}." -f ($csdn.'Database Connect', $DatabaseName))
         Set-ItemProperty -Path $registryPath -Name $registryKey -Value $DatabaseName
     }
     else
@@ -74,5 +75,65 @@ function Get-CSDNDatabase
     }
 }
 New-Alias -Name getcsdn -Value Get-CSDNDatabase -Description "Get the value of the CSDN database."
+
+function Update-CSDN
+{
+    <#
+        .SYNOPSIS
+        Update the local copy of the CSDN app.
+        
+        .DESCRIPTION
+        This cmdlet will update the local installation of CSDN.
+
+        .PARAMETER Clean
+        Perform a clean update by deleting the local copy first.
+
+        .PARAMETER Force
+        If CSDN is running, use this parameter to kill it without prompting.
+
+        .NOTES
+        NAME        :  Update-CSDN
+        VERSION     :  1.0   
+        LAST UPDATED:  2018-02-23
+        AUTHOR      :  Michael Metcalfe
+        .INPUTS
+        None
+        .OUTPUTS
+        None
+    #>
+    [CmdletBinding()]
+    param
+    (
+        [switch]$Clean,
+        [switch]$Force
+    )
+    $LOCAL_INSTALLATION_DIRECTORY = "C:\corp\csdn\MAINT_FT_FLDR\bin"
+    $REMOTE_INSTALLATION_DIRECTORY = "\\VAHCMN01\CDE\corp\CSDN\MAINT_FT_FLDR\bin"
+
+    if (Get-Process -Name "csdn")
+    {
+        Add-Type -AssemblyName System.Windows.Forms;
+        $response = [System.Windows.Forms.MessageBox]::Show('CSDN is running.  Do you want to kill it?', 'Warning', 3, 'Question')
+
+        if ($response -eq 'Yes')
+        {
+            Stop-Process -Name "csdn" -Force -ErrorAction Stop;
+        }
+        elseif ($response -eq 'Cancel')
+        {
+            return;
+        }
+    }
+
+    if ($Clean)
+    {
+        Write-Debug "Cleaning local copy...";
+        Remove-Item -Recurse -Force -Path $LOCAL_INSTALLATION_DIRECTORY;
+        New-Item -Type Directory $LOCAL_INSTALLATION_DIRECTORY | Out-Null;
+    }
+
+    Write-Host -ForegroundColor Cyan "Copying CSDN files...";
+    Copy-Item -Path "$REMOTE_INSTALLATION_DIRECTORY\*" -Destination $LOCAL_INSTALLATION_DIRECTORY -Recurse -ErrorAction Continue
+}
 
 Export-ModuleMember -Function * -Alias *
